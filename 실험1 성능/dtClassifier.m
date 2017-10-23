3074f74ffe01f55823c8868ebf162b3fd562bf32
Unknown <todaudrhdgkr@gmail.com>
2017-10-23 14:06:21 +0900

    dtClassifier.m의 함수명 수정
    nbClassifier -> dtClassifier

--- 실험1 성능/dtClassifier.m
+++ 실험1 성능/dtClassifier.m
@@ -1,4 +1,7 @@
-function res = nbClassifier(data, answer ,sim_seq)
+function res = dtClassifier(data, answer ,sim_seq)
+% nb = Decision Tree
+% multiclass classification 을 위해서 매트랩의 fitcree를 사용하였다.
+
 
 sim_num = size(sim_seq, 2);
 res = zeros(sim_num, 1);
@@ -9,7 +12,6 @@ for i = 1:sim_num
     ts_data = data(~sim_seq(:,i),:);
     ts_ans = answer(~sim_seq(:,i),:);
     
-    %mdl = fitcnb(tr_data, tr_ans, 'DistributionName', 'mvmn');
     mdl = fitctree(tr_data,tr_ans);
     pre = mdl.predict(ts_data);
     
