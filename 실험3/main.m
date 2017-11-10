215a20e5efa14f342ff1738f44c0dd67cb83b7be
Unknown <todaudrhdgkr@gmail.com>
2017-11-10 16:23:46 +0900

    WIP--main.m
    main에서 particle swarm optimization 알고리즘을 사용했을때를 위해서 기반?을 다져놓는 코딩중
    아직 PSO를 코딩중이지는 않음

--- 실험3/main.m
+++ 실험3/main.m
@@ -6,28 +6,51 @@ clear all; clc;
 
 
 
-% p = gcp('nocreate'); % If no pool, do not create new one.
-% if isempty(p)
-%     p = parpool(12); 
-% else
-%     delete(p);
-%     p = parpool(12); 
-% end
+p = gcp('nocreate'); % If no pool, do not create new one.
+if isempty(p)
+    p = parpool(feature('numcores')); 
+    
+else
+    delete(p);
+    p = parpool(feature('numcores')); 
+end
 
 dataFileLists = getDataLists('data');
 sizeofdataLists = length(dataFileLists);
+
+% Load 나 Gen 밖에 안된다.
 sequence = Sequence('Load',dataFileLists);
 
 
 
-answer = cell(11,1);
-for i = 1:sizeofdataLists
-   answer(i,1) = {["0" "0" "0" "0"]};
-end
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% % % % 앞부분 임시로 작성중
 DDDend = 9999;
-
 for k = 1:DDDend
     data = load(strcat(dataFileLists(k).folder,"\",dataFileLists(k).name));
     features = data.X;
@@ -40,24 +63,36 @@ for k = 1:DDDend
     a=1;
 
 end
+% % % % 
 
-ppm = ParforProgMon('진행도... ', sizeofdataLists)
 
-parfor k = 1:sizeofdataLists
-    data = load(strcat(dataFileLists(k).folder,"\",dataFileLists(k).name));
-    
-    temp = ["0" "0" "0" "0"];
-    for i = 1:4
-        
-        result = classifer(data.X, data.Y , sequence{k,1},i);
-        resultA = strcat(num2str(mean(result(:,1))), "±", num2str(std(result(:,1))));
-        temp(i) = resultA;
-        
-    end
-    answer(k) = {temp};
-    
-    ppm.increment();
-end
+
+
+
+
+% answer = cell(11,1);
+% for i = 1:sizeofdataLists
+%    answer(i,1) = {["0" "0" "0" "0"]};
+% end
+% 
+% 
+% ppm = ParforProgMon('진행도... ', sizeofdataLists)
+% 
+% parfor k = 1:sizeofdataLists
+%     data = load(strcat(dataFileLists(k).folder,"\",dataFileLists(k).name));
+%     
+%     temp = ["0" "0" "0" "0"];
+%     for i = 1:4
+%         
+%         result = classifer(data.X, data.Y , sequence{k,1},i);
+%         resultA = strcat(num2str(mean(result(:,1))), "±", num2str(std(result(:,1))));
+%         temp(i) = resultA;
+%         
+%     end
+%     answer(k) = {temp};
+%     
+%     ppm.increment();
+% end
 
 
 
